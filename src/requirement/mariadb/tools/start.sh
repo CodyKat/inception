eef6f930c91383415e5493ce4d7ec5262348dc61
jaemjeon <jaemjeon@student.42seoul.kr>
2023-06-22 10:33:18 +0900

    remove conf file, edit script

--- src/requirement/mariadb/tools/start.sh
+++ src/requirement/mariadb/tools/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 STATUS="$(service mariadb status)"
+echo "try booting mariadb.."
 if [ "${STATUS}" != "MariaDB is stopped.." ]; then
     echo "mariadb is running"
 else
-    service mariadb start
     while :
     do
+        service mariadb start
         STATUS="$(service mariadb status)"
         if [ "${STATUS}" != "MariaDB is stopped.." ]; then
             break
