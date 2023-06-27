95d33728d5f77ed1b9f61b52d48d6b3d1546ad28
jaemjeon <jaemjeon@student.42seoul.kr>
2023-06-27 13:27:22 +0900

    tmp

--- src/requirement/wordpress/tools/start-script.sh
+++ src/requirement/wordpress/tools/start-script.sh
@@ -13,6 +13,13 @@ else
 	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config-sample.php
 	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
+
+#	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#	chmod +x wp-cli.phar
+#	mv wp-cli.phar /usr/local/bin/wp
+#	wp core install --admin_user=rooty --admin_email=rooty@gmail.com --url=http://jaemjeon.42.fr --title=helloMyWorld
+#	wp-cli user create jaemjeon jaemjeon@gmail.com --role=author
+#	wp-cli user update 1 --user_pass=$WORDPRESS_DB_USER
 fi
 chown -R www-data:www-data /var/www/html
 exec php-fpm7.4 -F
