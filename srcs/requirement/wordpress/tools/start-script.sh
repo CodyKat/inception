f5b5acd8d682447400a5e289e6b11d7d54f9b2b8
jaemjeon <jaemjeon@student.42seoul.kr>
2023-06-27 18:43:46 +0900

    tmp. TODO: install wordpress and set volume

--- srcs/requirement/wordpress/tools/start-script.sh
+++ srcs/requirement/wordpress/tools/start-script.sh
@@ -17,9 +17,9 @@ else
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
-	wp core install --admin_user=rooty --admin_email=rooty@gmail.com --url=http://jaemjeon.42.fr --title=helloMyWorld
-	wp user create jaemjeon jaemjeon@gmail.com --role=author
-	wp user update 1 --user_pass=$WORDPRESS_DB_USER
+	wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_email=$WORDPRESS_ADMIN_USER@gmail.com --url=http://$WORDPRESS_USER.42.fr --title=$WORDPRESS_TITLE --allow-root
+	wp user create jaemjeon jaemjeon@gmail.com --role=author --allow-root
+	wp user update 2 --user_pass=$WORDPRESS_DB_USER --allow-root
 fi
 chown -R www-data:www-data /var/www/html
 exec php-fpm7.4 -F
